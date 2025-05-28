45953c44d4ba421b97b19a7d0c30cf34f4a78b66
kareemalaa010 <kareemalaaaldin@gmail.com>
2025-05-29 00:03:48 +0300

    finish today and upload before move the pcs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import { FrappeProvider } from 'frappe-react-sdk'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return ( 
 	<div className="App">
 	  <FrappeProvider>
 		<div>
@@ -14,12 +14,12 @@ function App() {
 		  <img src={reactLogo} className="logo react" alt="React logo" />
 		</a>
 	  </div>
-	  <h1>Vite + React + Frappe</h1>
+	  <h1>Vite + React </h1>
 	  <div className="card">
 		<button onClick={() => setCount((count) => count + 1)}>
 		  count is {count}
 		</button>
-		<p>
+		<p>   
 		  Edit <code>src/App.jsx</code> and save to test HMR
 		</p>
 	  </div>
